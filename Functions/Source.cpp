196c8932b4a0883d445cc8b354a83388fb9af40d
Bulu_fj20@itstepru.onmicrosoft.com <Bulu_fj20@itstepru.onmicrosoft.com>
2021-02-28 16:31:54 +0400

    Functions_Day_1

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -1,8 +1,59 @@
 #include<iostream>
 using namespace std;
+#define tab "\t"
+
+void FillRand(int arr[], const int n)
+{
+	//Заполняем массив случайными числами;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	// Выводим массив на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+
+void ShiftLeft(int arr[], const int n, const int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int j = 0; j < n; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "Hello Functions!" << endl;
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	// Выводим массив на экран:
+	Print(arr, n);
+	//Сдвиг массива на заданное число элементов:
+	int number_of_shifts;
+	cout << "Введите колличество сдвигов:"; cin >> number_of_shifts;
+	ShiftLeft(arr, n, number_of_shifts);
+	Print(arr, n);
+
+	const int m = 8;
+	int brr[m];
+	FillRand(brr, m);
+	Print(brr, m);
+	cout << "Введите колличество сдвигов:"; cin >> number_of_shifts;
+	ShiftLeft(brr, m, number_of_shifts);
+	Print(brr, m);
 }
